6b3055cd13fdae144230ffdc8ca7cb22570e88ba
MichaelSilski <sil16005@byui.edu>
2021-03-15 14:17:37 -0600

    Creating repository, adding files. Hope this works!

---
+++ app/src/main/java/com/example/searchviewtest/ListViewItems.java
@@ -0,0 +1,14 @@
+package com.example.searchviewtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListViewItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_items);
+    }
+}
