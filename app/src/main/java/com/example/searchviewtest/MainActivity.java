ec377f08370bdfb987ffd6a77360072ba7415815
MichaelSilski <sil16005@byui.edu>
2021-03-15 23:19:32 -0600

    Creating repository, adding files. Hope this works!

--- app/src/main/java/com/example/searchviewtest/MainActivity.java
+++ app/src/main/java/com/example/searchviewtest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.searchviewtest;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.ListView;
 import android.widget.SearchView;
 
